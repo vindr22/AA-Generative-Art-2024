449c831f55882fda47d79418f3ad4d8590ce5e39
vindr22 <105640722+vindr22@users.noreply.github.com>
2024-01-25 08:46:30 +0700

    add animation to the sun
    gave up on adding sunbeams

--- Sketch.js
+++ Sketch.js
@@ -2,21 +2,31 @@ function setup() {
   CANVAS_HEIGHT = 400
   CANVAS_WIDTH = 600
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  light_H = 400
-  light_W = 600
-  yCoor = 0
-
+  sun = 400
+  
 }
 
 function draw() {
   background(0, 0, 87);
   fill(254, 255, 141)
-  for (var i = 0; i < 600; i += 30) {
-    //debug line 16 plz
-    triangle(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, i, i, i, i+30)
+
+  //this is where i gave up on adding sun beam
+  //i asked yoyo to add radients around the sun instead
+  //for (var i = 0; i < 600; i += 30) {
+    //triangle(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, i, i, i, i+30)
+  //}
+  
+  noStroke();
+  fill(255, 141, 18);
+  if(sun>144){
+    circle(300, sun--, 150);
   }
-  fill(255, 105, 65)
-  circle(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, 225);
-  fill(15, 117, 246)
-  rect(0, CANVAS_HEIGHT/2, CANVAS_WIDTH, CANVAS_HEIGHT/2)
+  else if(sun==144){
+    circle(300, sun, 150);
+  }  
+
+  fill(10, 56, 226)
+  rect(0, CANVAS_HEIGHT/2-40, CANVAS_WIDTH, CANVAS_HEIGHT/2+40)
+  noFill();
+  
 }
