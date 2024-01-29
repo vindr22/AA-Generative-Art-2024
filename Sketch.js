f81886c368e7c4817ef851aa451cf3f489b2be45
vindr22 <105640722+vindr22@users.noreply.github.com>
2024-01-29 07:59:32 +0700

    add movement to the ocean

--- Sketch.js
+++ Sketch.js
@@ -1,32 +1,48 @@
+let yoff = 0.0; // 2nd dimension of perlin noise
+sun = 250
+
 function setup() {
-  CANVAS_HEIGHT = 400
-  CANVAS_WIDTH = 600
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  sun = 400
-  
+  createCanvas(600, 400);
 }
+
 
 function draw() {
   background(0, 0, 87);
-  fill(254, 255, 141)
-
-  //this is where i gave up on adding sun beam
-  //i asked yoyo to add radients around the sun instead
-  //for (var i = 0; i < 600; i += 30) {
-    //triangle(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, i, i, i, i+30)
-  //}
-  
   noStroke();
   fill(255, 141, 18);
-  if(sun>144){
+  if(sun>215){
     circle(300, sun--, 150);
   }
-  else if(sun==144){
+  else if(sun==215){
     circle(300, sun, 150);
   }  
 
-  fill(10, 56, 226)
-  rect(0, CANVAS_HEIGHT/2-40, CANVAS_WIDTH, CANVAS_HEIGHT/2+40)
-  noFill();
+  fill(10, 56, 226);
+  // We are going to draw a polygon out of the wave points
+  beginShape();
+
+  let xoff = 0; // Option #1: 2D Noise
+  // let xoff = yoff; // Option #2: 1D Noise
+
+  // Iterate over horizontal pixels
+  for (let x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to
+
+    // Option #1: 2D Noise
+    let y = map(noise(xoff, yoff), 0, 1, 200, 300);
+
+    // Option #2: 1D Noise
+    // let y = map(noise(xoff), 0, 1, 200,300);
+
+    // Set the vertex
+    vertex(x, y);
+    // Increment x dimension for noise
+    xoff += 0.05;
+  }
+  // increment y dimension for noise
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
   
 }
